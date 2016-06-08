1661ac35d638ab2f2bcbce5c50fa8751a5305dd1
k-kitai <kitai.koki@gmail.com>
2016-06-08 10:08:11 +0000

    bootstrap navbar

--- ec2/views.py
+++ ec2/views.py
@@ -4,3 +4,6 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return render(request, "ec2/index.html")
+
+def container_view(request):
+    return render(request, "ec2/container.html")
