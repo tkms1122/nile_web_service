cbac4c791651cc8dbf72f76f07980f4c216b89ce
KoheiOhara <gofue0723@gmail.com>
2016-06-10 19:35:40 +0900

    [add][未動作] ログインページ追加

---
+++ accounts/urls.py
@@ -0,0 +1,10 @@
+from django.conf.urls import patterns, include, url
+from accounts import views
+
+urlpatterns = patterns('',
+    # url(r'^profile/$', views.user_profile, name='user_profile'),
+    url(r'^login/$', 'django.contrib.auth.views.login',
+        {'template_name': 'accounts/login.html'}),
+    url(r'^logout/$', 'django.contrib.auth.views.logout',
+        {'template_name': 'accounts/logged_out.html'}),
+)
