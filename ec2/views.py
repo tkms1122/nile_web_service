bebbab6b0c06beed34ea69a68fb86f6123e1d46c
t1ks0n <yusuke.3.tokunaga@gmail.com>
2016-06-10 12:21:48 +0900

    implemented tab&terminal

--- ec2/views.py
+++ ec2/views.py
@@ -1,6 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, render_to_response
+from django.template import RequestContext
 from django.http import HttpResponse
 
 # Create your views here.
 def main(request):
     return HttpResponse("Hello EC2!")
+
+def tab(request):
+    return render_to_response('ec2/tab.html', None , context_instance=RequestContext(request))
