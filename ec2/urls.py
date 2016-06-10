3476b38eeee84b8ceff508b8fed091abfcbf1dda
t1ks0n <yusuke.3.tokunaga@gmail.com>
2016-06-10 21:39:53 +0900

    add route to urls.py

--- ec2/urls.py
+++ ec2/urls.py
@@ -2,6 +2,11 @@ from django.conf.urls import patterns, url
 from ec2 import views
 
 urlpatterns = patterns('',
-                       url(r'^$', views.main, name='main'),
-                       url(r'^tab/$', views.tab, name='tab'),
+                       url(r'^$', views.machines_index),
+                       url(r'^home/$', views.machines_index),
+                       url(r'^machines/$', views.machines_index),
+                       # url(r'^machines/(\d+)$', views.machines_show),
+                       # url(r'^machines/new$', views.machines_new),
+                       # url(r'^machines/create$', views.machines_create), POST
+                       # url(r'^machines/destroy$', views.machines_destroy), POST
                        )
--- nile_web_service/urls.py
+++ nile_web_service/urls.py
@@ -17,7 +17,7 @@ from django.conf.urls import url, include
 from django.contrib import admin
 
 urlpatterns = [
+    # url(r'^$', ),TODO
     url(r'^admin/', admin.site.urls),
-    # url(r'^ec2/', 'ec2.views.main'),
     url(r'^ec2/', include('ec2.urls', namespace='ec2')),
 ]
