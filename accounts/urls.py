751365a4002db0de839d83ad2873384bc07216b6
KoheiOhara <gofue0723@gmail.com>
2016-06-17 19:30:06 +0900

    [add, change] ログイン機能追加．ファイルを移動．ユーザ変更機能追加（アクセスの仕方はわからず）

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,9 +4,9 @@ from django.contrib.auth import views as auth_views
 
 urlpatterns = patterns('',
     # url(r'^profile/$', views.user_profile, name='user_profile'),
-    url(r'^login/$', 'django.contrib.auth.views.login',
-        {'template_name': 'accounts/login.html'}),
+    url(r'^login/$', 'django.contrib.auth.views.login'),
     url(r'^logout/$', 'django.contrib.auth.views.logout',
         {'template_name': 'accounts/logged_out.html'}),
     url(r'^user-creation/$', views.AccountCreateView.as_view(), name='user_creation'),
+    url(r'^(?P<pk>[0-9]+)/update/$', views.AccountUpdateView.as_view(), name='user_change'),
 )
--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
+from django.views.generic import CreateView, UpdateView
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+from django.contrib.auth.models import User
 
 # Create your views here.
+# def login(request):
+#     return render(request, reverse("accounts:login"))
+
+
+# def signup(request):
+#     return render(request, reverse("accounts:signup"))
+
+class AccountCreateView(CreateView):
+    model = User
+
+    form_class = UserCreationForm
+
+    def get_success_url(self):
+        return reverse('my:user_creation')
+
+class AccountUpdateView(UpdateView):
+    model = User
+    form_class = UserChangeForm
+
+    def get_success_url(self):
+        return reverse('my:user_change', args=(self.object.id, ))
