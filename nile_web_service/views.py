752e24dba633c4392ff467a150c51b8ed90562ee
t1ks0n <yusuke.3.tokunaga@gmail.com>
2016-06-26 13:50:10 +0900

    merge

--- nile_web_service/views.py
+++ nile_web_service/views.py
@@ -3,7 +3,10 @@ from django.template import RequestContext
 from django.http import HttpResponse
 from django.core.context_processors import csrf
 from django.contrib.auth.decorators import login_required
+from ec2.views import RegistrationForm, LoginForm
 
 def welcome(request):
-    return render_to_response('nile_web_service/index.html', None, context_instance=RequestContext(request))
+    s_form = RegistrationForm()
+    l_form = LoginForm()
+    return render_to_response('nile_web_service/index.html', {'singin': s_form,'login' : l_form}, context_instance=RequestContext(request))
 
