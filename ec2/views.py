fbf23ad6fdfe8a8e9f7241f66964d82ef8bd4b57
KoheiOhara <gofue0723@gmail.com>
2016-06-26 18:06:39 +0900

    途中まででcommit

--- ec2/views.py
+++ ec2/views.py
@@ -3,6 +3,7 @@ from django.template import RequestContext
 from django.http import HttpResponse
 from django.core.context_processors import csrf
 from .models import Machine
+from .models import IP
 from django.contrib.auth.decorators import login_required
 from django import forms
 import json, uuid, os
@@ -24,34 +25,47 @@ def machines_index(request):
     return render_to_response('ec2/machines/index.html', {'machines': machines,'singup': s_form,'login' : l_form} , context_instance=RequestContext(request))
 
 def machines_launch(request):
-    def validate(name,core,mem,token):
-        return (len(name) > 3)
+    def validate(name,core,mem,token,ip):
+        # FIXED
+        if ip == None:
+            return False
+        elif len(name) <= 3:
+            return False
+        else:
+            return True
+
     res = {}
     if request.user.is_authenticated():
         machine_token = uuid.uuid4()
         machine_name = request.GET['machine_name']
         cpu_core = request.GET['cpu_core']
         memory_size = request.GET['memory']
-        isvalid = validate(machine_name, cpu_core, memory_size, machine_token)
+        # FIXED
+        #q = IP.objects.filter(is_used=False)[:1]
+        ip = 'object' # is_usedじゃないレコードのオブジェクト
+
+        isvalid = validate(machine_name, cpu_core, memory_size, machine_token, ip)
         res['isvalid'] = isvalid
         if isvalid:
-            m = Machine(auth_user=request.user, machine_token=machine_token, name=machine_name, core=cpu_core,
+            m = Machine(auth_user=request.user, ip=ip, machine_token=machine_token, name=machine_name, core=cpu_core,
                         memory=memory_size, status=0)
             res['name'] = machine_name
             res['core'] = cpu_core
             res['stat'] = m.getstate()
             res['statcolor'] = m.getstatecolor()
             m.save()
+            ip.is_used = True
+            ip.save()
             # 以下でsshkeyのペアを作成
             ssh_key_name = '{0}_{1}'.format(request.user.username, machine_name)
             os.system('ssh-keygen -b 4096 -t rsa -N "" -f /tmp/{0}'.format(ssh_key_name))
             os.system('scp /tmp/{0}.pub cloudA1-2:/tmp/{0}.pub'.format(ssh_key_name))
             os.system('rm /tmp/{0}.pub'.format(ssh_key_name))
-            # os.system('ssh cloudA1-2 "sudo bash /home/nws/create.bash {vm_name} {pub_key} {ip}"'.format(
-            #     vm_name=machine_token,
-            #     pub_key='/tmp/{0}.pub'.format(ssh_key_name),
-            #     ip='hoge'
-            # ))
+            os.system('ssh cloudA1-2 "sudo bash /home/nws/create.bash {vm_name} {pub_key} {ip}"'.format(
+                vm_name=machine_token,
+                pub_key='/tmp/{0}.pub'.format(ssh_key_name),
+                ip=ip.address
+            ))
     return HttpResponse(json.dumps(res))
     
 def machines_destroy(request, machine_token):
