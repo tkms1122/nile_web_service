4d42d40a95c89e5f72a501471aef293d465e2b02
nws <nws@example.com>
2016-06-28 02:13:02 +0900

    update launch VM

--- ec2/views.py
+++ ec2/views.py
@@ -64,9 +64,9 @@ def machines_launch(request):
             # 鍵生成&VM立ち上げ
             ssh_key_name = '{0}_{1}'.format(request.user.username, machine_name)
             os.system('ssh-keygen -b 4096 -t rsa -N "" -f /tmp/{0}'.format(ssh_key_name))
-            os.system('scp /tmp/{0}.pub cloudA1-2:/tmp/{0}.pub'.format(ssh_key_name))
+            os.system('scp -i /home/nws/.ssh/id_rsa /tmp/{0}.pub nws@157.82.3.112:/tmp/{0}.pub'.format(ssh_key_name))
             os.system('rm /tmp/{0}.pub'.format(ssh_key_name))
-            os.system('ssh cloudA1-2 "sudo bash /home/nws/create.bash {vm_name} {pub_key} {ip}"'.format(
+            os.system('ssh -i /home/nws/.ssh/id_rsa nws@157.82.3.112 "sudo bash /home/nws/create.bash {vm_name} {pub_key} {ip}"'.format(
                 vm_name = machine_token,
                 pub_key = '/tmp/{0}.pub'.format(ssh_key_name),
                 ip = ip.address
